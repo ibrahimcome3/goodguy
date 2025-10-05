c71d411f448228985b7d2618e1f5ff6458bd708f
Ibrahim Ahmad Muhammad <47833263+ibrahimcome3@users.noreply.github.com>
2025-10-05 21:46:14 +0100

    Change MySQL connection details in includes.php
    Updated database connection credentials for MySQL.

--- includes.php
+++ includes.php
@@ -1,9 +1,13 @@
 <?php
-
+$db   = 'u633821528_goodguyng';
+      $user = 'u633821528_goodguyng';
+      $pass = 'PPassword12@';
+      $charset = 'utf8mb4';
 $host = "localhost"; // Host name
-$username = "root";
-$password = ""; // Mysql password
-$db_name = "lm_test"; // Database name
+$username = "u633821528_goodguyng";
+$password = "PPassword12@"; // Mysql password
+$db_name = "u633821528_goodguyng"; // Database name
+
 $mysqli = mysqli_connect("$host", "$username", "$password", "$db_name") or die("cannot connect");
 if (mysqli_connect_errno()) {
      echo "Failed to connect to MySQL: " . mysqli_connect_error();
